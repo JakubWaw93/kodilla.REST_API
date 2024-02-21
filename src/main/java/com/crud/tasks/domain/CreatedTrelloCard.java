eb8bf9bcb496f47e5bd6cda1d7b198c111128753
Jakub Wawryniuk <jakubwaw93@gmail.com>
2024-02-21 18:39:18 +0100

    21.3 extending the response class

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -4,18 +4,16 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class CreatedTrelloCard {
 
-    @JsonProperty("name")
-    private String name;
     @JsonProperty("id")
     private String id;
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists = new ArrayList<>();
-
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("shortUrl")
+    private String shortUrl;
+    @JsonProperty("badges")
+    private Badges badges;
 }
