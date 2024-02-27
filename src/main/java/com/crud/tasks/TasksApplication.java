552772abc943601568762423df833caf2dec3771
Jakub Wawryniuk <jakubwaw93@gmail.com>
2024-02-27 19:43:13 +0100

    22.3 mailtrap

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,11 +1,13 @@
 package com.crud.tasks;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class TasksApplication {
 
 	public static void main(String[] args) {
