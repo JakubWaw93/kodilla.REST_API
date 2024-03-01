0da7335af96bff9abcd5536f5cbe832a301fc9ce
Jakub Wawryniuk <jakubwaw93@gmail.com>
2024-03-01 19:53:02 +0100

    22.3 Adding mail to messages (CC)

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,14 @@
+package com.crud.tasks.domain;
+
+import lombok.*;
+
+@Getter
+@Builder(access = AccessLevel.PUBLIC)
+public class Mail {
+
+    private final String mailTo;
+    private final String subject;
+    private final String message;
+    private final String toCC;
+
+}
---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,51 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class SimpleEmailServiceTest {
+
+    @InjectMocks
+    private SimpleEmailService service;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    void shouldSendEmail() {
+        //Given
+        Mail mail = Mail.builder()
+                .mailTo("test@test.com")
+                .subject("Test")
+                .message("Test  message")
+                .build();
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setText(mail.getMessage());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setTo(mail.getMailTo());
+
+        if (Optional.ofNullable(mail.getToCC()).isPresent()) {
+            mailMessage.setCc(mail.getToCC());
+        }
+
+        //When
+        service.send(mail);
+
+        //Then
+        verify(javaMailSender, times(1)).send(mailMessage);
+    }
+}
